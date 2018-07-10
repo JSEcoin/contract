245baaad2ebfb440a2ff44872e1601a5d94c7112
Amr Gawish <amr.gawish@gmail.com>
2018-07-10 12:45:29 +0100

    Adding the correct figures for the ICO launch

--- contracts/JSECoinCrowdsaleConfig.sol
+++ contracts/JSECoinCrowdsaleConfig.sol
@@ -7,7 +7,7 @@ contract JSECoinCrowdsaleConfig {
 
     uint256 public constant DURATION                                = 12 weeks; 
     uint256 public constant CONTRIBUTION_MIN                        = 0.1 ether; // Around $64
-    uint256 public constant CONTRIBUTION_MAX_NO_WHITELIST           = 15.8 ether; // $10,000
+    uint256 public constant CONTRIBUTION_MAX_NO_WHITELIST           = 20 ether; // $9,000
     uint256 public constant CONTRIBUTION_MAX                        = 10000.0 ether; //After Whitelisting
     
     uint256 public constant TOKENS_MAX                              = 10000000000 * (10 ** uint256(TOKEN_DECIMALS)); //10,000,000,000 aka 10 billion
@@ -16,9 +16,9 @@ contract JSECoinCrowdsaleConfig {
 
 
     // For the public sale, tokens are priced at 0.006 USD/token.
-    // So if we have 630 USD/ETH -> 630,000 USD/KETH / 0.006 USD/token = ~105000000
+    // So if we have 450 USD/ETH -> 450,000 USD/KETH / 0.006 USD/token = ~75000000
                                                                     //    3600000
-    uint256 public constant TOKENS_PER_KETHER                       = 105000000;
+    uint256 public constant TOKENS_PER_KETHER                       = 75000000;
 
     // Constant used by buyTokens as part of the cost <-> tokens conversion.
     // 18 for ETH -> WEI, TOKEN_DECIMALS (18 for JSE Coin Token), 3 for the K in tokensPerKEther.
